3aa73c0baac14c074655719555997da88c97befe
burnjoe <sabanajholo@gmail.com>
2024-04-17 11:41:10 +0800

    add user migration + model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->text('image')->nullable();
+            $table->timestamp('email_verified_at')->nullable(); // email_verified
             $table->string('password');
+            $table->enum('account_type', ['Writer']);
+            $table->string('provider');
             $table->rememberToken();
             $table->timestamps();
         });

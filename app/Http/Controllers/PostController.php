9d2aceaac4e4ab0dbd725a349a29125f53c154a2
burnjoe <sabanajholo@gmail.com>
2024-04-22 00:47:16 +0800

    add Get all method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    
+    /**
+     * Validation rules
+     * 
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => 'required',
+            'description' => 'required',
+            'image' => '',
+            'status' => 'required',
+            'writer_id' => 'required',
+            'category_id' => 'required',
+        ];
+    }
+
+
+    /**
+     * Get all users
+     * 
+     * @return Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        try {
+            // Retrieve All Users
+            $user = Post::with('writer', 'category')
+                ->get();
+
+            return response()->json([
+                'success' => true,
+                'data' => $user
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Internal server error'
+            ], 500);
+        }
+    }
 }

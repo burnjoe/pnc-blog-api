fba471d7980f3fa8d058f136c7fc2d2041343139
burnjoe <sabanajholo@gmail.com>
2024-04-22 00:28:31 +0800

    add category and post migration + relationships

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -15,6 +16,17 @@ class Category extends Model
      * @var array<int, string>
      */
     protected $fillable = [
-        
+        'name',
     ];
+
+
+    /**
+     * Category has many posts
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'category_id', 'id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -23,4 +24,15 @@ class Post extends Model
         'user_id',
         'category_id',
     ];
+
+
+    /**
+     * Post belongs to category
+     * 
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
